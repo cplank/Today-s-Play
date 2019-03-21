0ee2426f1e3c7013eeba062a42b433b183c57e4a
cplank <cplankshepard@gmail.com>
2019-03-21 16:02:19 -0700

    testing local storage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,7 +6,7 @@ console.log(addDay);
 
 // GLOBAL VARS
 let currentDate = moment().format('LL'); // grabs current date from m.js in mmmm dd, yyyy format
-
+let formattedCurrentDate = moment().format("YYYY-MM-DD");
 
 let userCity;
 
@@ -217,6 +217,11 @@ function renderTMEvents(startDate, startTime, endDate, endTime, city, state, pos
 function userAction() {
 
     userCity = $("#user-input").val().trim(); // grab user input for City
+    // localStorage.setItem("location", userCity);
+    // localStorage.setItem("date", formattedCurrentDate);
+
+    // userCity = localStorage.getItem("location");
+    // formattedCurrentDate = localStorage.getItem("date");
 
 
     $("#widgets").removeClass("hidden"); // shows widget section
@@ -319,7 +324,7 @@ function inputTodayDate() {
 }
 function addDayInWidget() {
 
-    let spotyWidgy = '<div w-type="event-discovery" w-tmapikey="HuptMNvrDLaDMhz8Y5NOpg5s7hvSDucs" w-googleapikey="AIzaSyAt-7vjGZ8A-EuZhf1F_AJCUkGU3Zsky_o" w-keyword="" w-theme="listviewthumbnails" w-colorscheme="dark" w-width="350" w-height="500" w-size="25" w-border="2" w-borderradius="4" w-postalcode="" w-radius="25" w-city="Seattle" w-period="custom" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="custom" w-titlelink="off" w-sorting="groupByName" w-id="id_o1oh7a" w-countrycode="US" w-source="" w-classificationname="music" w-startdatetime=' + addDay + ' w-enddatetime=' + addDay + ' w-latlong=""></div>'
+    let spotyWidgy = '<div w-type="event-discovery" w-tmapikey="HuptMNvrDLaDMhz8Y5NOpg5s7hvSDucs" w-googleapikey="AIzaSyAt-7vjGZ8A-EuZhf1F_AJCUkGU3Zsky_o" w-keyword="" w-theme="listviewthumbnails" w-colorscheme="dark" w-width="350" w-height="500" w-size="25" w-border="2" w-borderradius="4" w-postalcode="" w-radius="25" w-city=' + userCity + ' w-period="custom" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="custom" w-titlelink="off" w-sorting="groupByName" w-id="id_o1oh7a" w-countrycode="US" w-source="" w-classificationname="music" w-startdatetime=' + addDay + ' w-enddatetime=' + addDay + ' w-latlong=""></div>'
 
     $('#spotifywidgethole').html(spotyWidgy);
 
