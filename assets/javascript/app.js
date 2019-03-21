88c639dffb818258c875e0179487803ecbd46387
Jordan Babbitt <JBabbitt27@gmail.com>
2019-03-21 14:09:11 -0700

    Functional Widget with city load from userinput

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,12 +4,11 @@ console.log(todayDate)
 var addDay = moment().add(1, "days").format("YYYY-MM-DD");
 console.log(addDay);
 
+
 // GLOBAL VARS
 let currentDate = moment().format('LL'); // grabs current date from m.js in mmmm dd, yyyy format
 
-
-let userCity;
-
+// Getting userCity input in app.js file from local storage
 $("#todays-date").text(currentDate); // this changes the DOM's current date
 
 
@@ -314,12 +313,13 @@ $('#back-to-top').click(function (event) {  // when start button is clicked
 });
 
 //Using javascript to make the ticketmaster widget
+//Adding in user input for city in widget (+ userCity +)
 function inputTodayDate() {
 
 }
 function addDayInWidget() {
 
-    let spotyWidgy = '<div w-type="event-discovery" w-tmapikey="HuptMNvrDLaDMhz8Y5NOpg5s7hvSDucs" w-googleapikey="AIzaSyAt-7vjGZ8A-EuZhf1F_AJCUkGU3Zsky_o" w-keyword="" w-theme="listviewthumbnails" w-colorscheme="dark" w-width="350" w-height="500" w-size="25" w-border="2" w-borderradius="4" w-postalcode="" w-radius="25" w-city="Seattle" w-period="custom" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="custom" w-titlelink="off" w-sorting="groupByName" w-id="id_o1oh7a" w-countrycode="US" w-source="" w-classificationname="music" w-startdatetime=' + addDay + ' w-enddatetime=' + addDay + ' w-latlong=""></div>'
+    let spotyWidgy = '<div w-type="event-discovery" w-tmapikey="HuptMNvrDLaDMhz8Y5NOpg5s7hvSDucs" w-googleapikey="AIzaSyAt-7vjGZ8A-EuZhf1F_AJCUkGU3Zsky_o" w-keyword="" w-theme="listviewthumbnails" w-colorscheme="dark" w-width="350" w-height="500" w-size="25" w-border="2" w-borderradius="4" w-postalcode="" w-radius="25" w-city='+ userCity + ' w-period="custom" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="custom" w-titlelink="off" w-sorting="groupByName" w-id="id_o1oh7a" w-countrycode="US" w-source="" w-classificationname="music" w-startdatetime=' + addDay + ' w-enddatetime=' + addDay + ' w-latlong=""></div>'
 
     $('#spotifywidgethole').html(spotyWidgy);
 
